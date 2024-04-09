b7cd765772fb2e23b72662c412f7022d23bbae85
PeiYun <ollylee8520@gmail.com>
2024-04-09 13:30:47 -0600

    make x and y axis with d3

--- src/app/charts/chart3/chart3.component.ts
+++ src/app/charts/chart3/chart3.component.ts
@@ -18,12 +18,24 @@ import * as d3 from 'd3';
 export class Chart3Component implements OnInit, OnChanges {
   host: any;
   svg: any;
+  dataContainer: any;
+  xAxisContainer: any;
+  yAxisContainer: any;
   @Input() data;
   rectWidth = 30;
   padding = 3;
   dimensions: DOMRect;
+  left = 60;
+  right = 20;
+  bottom = 16;
+  top = 15;
+  innerWidth;
+  innerHeight;
+  xAxis: any;
+  yAxis: any;
   // from d3 document
-  x = d3.scaleLinear();
+  x = d3.scaleBand().paddingInner(0.2).paddingOuter(0.2);
+  y = d3.scaleLinear();
 
   constructor(private element: ElementRef) {
     this.host = d3.select(element.nativeElement);
@@ -35,29 +47,65 @@ export class Chart3Component implements OnInit, OnChanges {
     //this will select the first svg in the page (chart1) not the right one
     // this.svg = d3.select('svg');
     this.svg = this.host.select('svg');
+    this.setDimensions();
+    this.setElements();
+  }
+
+  setDimensions() {
     this.dimensions = this.svg.node().getBoundingClientRect();
+    this.innerWidth = this.dimensions.width - this.left - this.right;
+    this.innerHeight = this.dimensions.height - this.top - this.bottom;
+  }
+
+  setElements() {
+    this.dataContainer = this.svg
+      .append('g')
+      .attr('class', 'dataContainer')
+      .attr('transform', `translate(${this.left},${this.top})`);
+    this.xAxisContainer = this.svg
+      .append('g')
+      .attr('class', 'xAxisContainer')
+      .attr(
+        'transform',
+        `translate(${this.left},${this.top + this.innerHeight})`
+      );
+    this.yAxisContainer = this.svg
+      .append('g')
+      .attr('class', 'yAxisContainer')
+      .attr('transform', `translate(${this.left},${this.top})`);
   }
 
   ngOnChanges(changes: SimpleChanges) {
     if (!this.svg) return;
     this.setParams();
+    this.setAxis();
     this.draw();
   }
 
+  setAxis() {
+    this.xAxis = d3.axisBottom(this.x);
+    this.xAxisContainer.call(this.xAxis);
+
+    this.yAxis = d3.axisLeft(this.y);
+    this.yAxisContainer.call(this.yAxis);
+  }
+
   setParams() {
+    const ids = this.data.map((d) => d.id);
+    this.x.domain(ids).range([0, this.innerWidth]);
     const max_salary =
       1.3 * Math.max(...this.data.map((item) => item.employee_salary));
-    this.x.domain([0, max_salary]).range([this.dimensions.height, 0]);
+    this.y.domain([0, max_salary]).range([this.innerHeight, 0]);
   }
   draw() {
-    this.svg
+    this.dataContainer
       .selectAll('rect')
       .data(this.data || [])
       .enter()
       .append('rect')
-      .attr('x', (d, i) => i * (this.rectWidth + this.padding))
-      .attr('width', this.rectWidth)
-      .attr('height', (d) => this.dimensions.height - this.x(d.employee_salary))
-      .attr('y', (d) => this.x(d.employee_salary));
+      .attr('x', (d) => this.x(d.id))
+      .attr('width', this.x.bandwidth())
+      .attr('y', (d) => this.y(d.employee_salary))
+      .attr('height', (d) => this.innerHeight - this.y(d.employee_salary));
   }
 }
